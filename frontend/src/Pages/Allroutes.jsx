07317c6ba416739e77c39d4ae0a934babc208e1c
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-05-05 00:45:45 +0530

    add routes

--- frontend/src/Pages/Allroutes.jsx
+++ frontend/src/Pages/Allroutes.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Products from "./Product/Product";
+import Signup from "./Signup";
+import { BrandedDeals } from "./Cart/scroller";
+import AdminLogin from "./Admin Page/AdminLogin";
+import AddProducts from "./Admin Page/AddProducts";
 
 const Allroutes = () => {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Signup />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<BrandedDeals />} />
+        <Route path="/adminlogin" element={<AdminLogin />} />
+        <Route path="/adminaddproduct" element={<AddProducts />} />
       </Routes>
     </div>
   );
