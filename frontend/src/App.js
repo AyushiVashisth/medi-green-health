0ccc152b6995801c0eb2b7ba81e4e8dfa63dad80
akshay kumar <akshay123332@gmail.com>
2023-05-06 22:01:02 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Allroutes from "./Pages/Allroutes";
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <div className="App">
      <Allroutes/>
+     <Login/>
     </div>
   );
 }
