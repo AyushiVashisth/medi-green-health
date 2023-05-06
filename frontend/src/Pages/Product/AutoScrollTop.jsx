8d689f0e2ec8c4cb0a37dbc3769f492cc8002649
= <aajad@gmail.com>
2023-05-06 23:26:54 +0530

    hello_guys

---
+++ frontend/src/Pages/Product/AutoScrollTop.jsx
@@ -0,0 +1,37 @@
+import { Button } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { FaArrowCircleUp } from 'react-icons/fa';
+// import { Button } from './Styles';
+
+const ScrollButton = () => {
+
+    const [visible, setVisible] = useState(false)
+
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 150) {
+            setVisible(true)
+        }
+        else if (scrolled <= 150) {
+            setVisible(false)
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+            
+        });
+    };
+    // useEffect(())
+    window.addEventListener('scroll', toggleVisible);
+
+    return (
+        <Button onClick={scrollToTop} style={{padding:"10px", fontSize: "26px", width: "30px", height: "45px", backgroundColor: "#ff6f61", display: visible ? 'inline' : 'none', position: "absolute", top: "85%", left: "95%", position: "fixed",color:"#fff",textAlign:"center" }}>
+            ^
+        </Button>
+    );
+}
+
+export default ScrollButton;
