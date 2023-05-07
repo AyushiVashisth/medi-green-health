654e1f6aeaf55b0499e8ecc8ad21da911fb75dc2
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-05-07 11:33:44 +0530

    filter,sorting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./Components/Footer";
 import Allroutes from "./Pages/Allroutes";
 
 function App() {
   return (
     <div className="App">
      <Allroutes/>
+     <Footer/>
     </div>
   );
 }
