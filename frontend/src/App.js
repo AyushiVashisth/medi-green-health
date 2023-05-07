a55a3c41537ddee479f827652c46824b9b001c94
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-05-08 03:02:53 +0530

    Search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Footer from "./Components/Footer";
 import Allroutes from "./Pages/Allroutes";
+import ScrollButton from "./Pages/Product/AutoScrollTop";
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
      <Allroutes/>
      <Footer/>
-
+     <ScrollButton/>
     </div>
   );
 }
--- frontend/src/Components/Navbar/SearchBar.jsx
+++ frontend/src/Components/Navbar/SearchBar.jsx
@@ -5,28 +5,49 @@ import {
   Input,
   InputGroup,
   InputLeftAddon,
-  InputRightElement,
   Select,
   Text,
+  Link,
+  useBoolean,
 } from "@chakra-ui/react";
-// import React, { useContext, useState } from "react";
-import { FaSearch, FaSearchLocation } from "react-icons/fa";
-// import { SearchContext } from "../../Context/SearchContext";
-// import { useNavigate } from "react-router-dom";
-// const initial = "";
+import useThrottle from "../../Components/customHooks/useThrottle";
+import React, { useEffect, useState } from "react";
+import { FaSearchLocation } from "react-icons/fa";
 function SearchBar() {
-//   const { setQuery } = useContext(SearchContext);
-//   const [searchtext, setSearchext] = useState(initial);
-//   const navigate = useNavigate();
-
-//   const handleKeyDown = (e) => {
-//     if (e.key === "Enter") {
-//       setQuery(searchtext);
-//       setSearchext("");
-//       navigate("/products");
-//     }
-//   };
-
+  const [onChangeValue, setOnChangeValue] = useState("");
+  const [search, setSearch] = useState([]);
+  const [Products, setProducts] = useState([]);
+  const [showDropdown, setShowDropdown] = useBoolean();
+  const throttledText = useThrottle(onChangeValue, 400);
+  const searchMovies = async () => {
+    const data = await fetch(`https://onemg-database.onrender.com/vitamin`);
+    const res = await data.json();
+    setProducts(res);
+  };
+  useEffect(() => {
+    searchMovies();
+  }, []);
+  useEffect(() => {
+    if (throttledText === "") {
+      setSearch([]);
+    } else {
+      console.log(throttledText);
+      let newSuggestions = Products.filter((item) => {
+        return item.title
+          .split(" ")
+          .join("")
+          .trim()
+          .toLowerCase()
+          .indexOf(throttledText) !== -1
+          ? true
+          : false;
+      });
+      setSearch(newSuggestions);
+      setShowDropdown.on();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [throttledText]);
+  console.log(search);
   return (
     <Box fontFamily={"Clear-Sans"} shadow={"sm"} bg="#fff">
       <Flex justify={"space-around"} minH={"40px"} py="10px" align={"center"}>
@@ -47,21 +68,43 @@ function SearchBar() {
               <option value="Jaipur">Jaipur</option>
             </Select>
           </InputGroup>
-          <InputGroup size={"sm"} width={{ base: "90%", md: "60%" }}>
-            <Input
-              placeholder="Search for Medical and Health Products"
-              bg={"#f1f4f6"}
-              focusBorderColor="#f1f4f6"
-        //       onChange={(e) => setSearchext(e.target.value)}
-        //       onKeyDown={handleKeyDown}
-            />
-            <InputRightElement
-              children={<FaSearch color="green.500" />}
-              pointerEvents={"auto"}
-              cursor="pointer"
-        //       onClick={handleKeyDown}
-            />
-          </InputGroup>
+          <Input
+            ml={"20px"}
+            variant="filled"
+            placeholder="Search for medicines and health products"
+            size="sm"
+            w={"100%"}
+            onChange={(e) => setOnChangeValue(e.target.value)}
+          />
+          {search.length > 0 && (
+            <Box
+              borderRadius="5px"
+              position="absolute"
+              top="100px"
+              zIndex="100"
+              bgColor="white"
+              overflow="scroll"
+              w="38%"
+              maxH="400px"
+              m="auto"
+              left={"280"}
+            >
+              {search.map((item, i) => {
+                return (
+                  <Link href={`/products/${item._id}`} key={i + 1}>
+                    <Text
+                      fontSize="16px"
+                      p="10px"
+                      cursor="pointer"
+                      onClick={setShowDropdown.off}
+                    >
+                      {item.title}
+                    </Text>
+                  </Link>
+                );
+              })}
+            </Box>
+          )}
         </Flex>
 
         <Flex
